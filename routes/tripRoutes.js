560eca0ca4ac6487077f4a04a88bb497ed08e2ff
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-15 12:03:50 +0530

    rh

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const { requestTrip, acceptTrip, previousTrips } = require('../controllers/tripController');
 
-router.post('/request', requestTrip);
-router.post('/accept', acceptTrip);
-router.get('/history/:userId', previousTrips);
+router.post('/request', auth, requestTrip);            // Rider requests trip
+router.post('/accept', auth, acceptTrip);              // Driver accepts trip
+router.get('/history/:userId', auth, previousTrips);   // Retrieve trip history
 
 module.exports = router;
