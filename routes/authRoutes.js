76dcef72211347f9bc9d757a47d90f8b5994dc37
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-15 14:12:25 +0530

    rh

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth'); 
 const verifyToken = require('../middleware/auth');
+
+const tripController = require('../controllers/tripController');
 const { register, login, toggleDriverStatus } = require('../controllers/authController');
 
 // Register Route
@@ -13,4 +15,6 @@ router.post('/login', login);
 // Toggle Driver Online/Offline Status Route
 router.post('/driver-status', auth, verifyToken,toggleDriverStatus); // Use auth middleware
 
+// Protect the trip accept route with the verifyToken middleware
+router.post('/accept', verifyToken, tripController.acceptTrip);
 module.exports = router;
