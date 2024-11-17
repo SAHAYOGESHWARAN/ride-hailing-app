02cdfc4ade81fd2e67ffc500fc3749f0e4eac2cb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-11-17 21:49:15 +0530

    rh

--- models/User.js
+++ models/User.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema(
                 message: (props) => `${props.value} is not a valid email address`,
             },
         },
+        
         password: {
             type: String,
             required: [true, 'Password is required'],
